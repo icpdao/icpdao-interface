762ce46059562ca357eb07402f541fdba5df1c1f
bqx619 <bqx619@gmail.com>
2021-05-14 10:22:29 +0800

    fix: had mentor access

--- src/access.ts
+++ src/access.ts
@@ -7,6 +7,6 @@ export default function access() {
   return {
     isNormal: getUserInfo().profile,
     hadAccept: getUserInfo().profile?.icppership?.progress !== 0,
-    hadMentor: getUserInfo().profile?.progress !== 0 && getUserInfo().profile?.icppership?.mentor?.github_login,
+    hadMentor: getUserInfo().profile?.icppership?.progress !== 0 && getUserInfo().profile?.icppership?.mentor?.github_login,
   };
 }
