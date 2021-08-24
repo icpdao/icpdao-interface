ae1a875785de30faac7bfe4e606d0f9aca24ad5d
bqx619 <bqx619@gmail.com>
2021-08-25 02:00:00 +0800

    feat: dao icpper page

--- src/pages/Dao/components/DaoIcpperStat.tsx
+++ src/pages/Dao/components/DaoIcpperStat.tsx
@@ -1,13 +1,151 @@
-import React from 'react';
-import { Empty } from 'antd';
+import React, { useCallback, useState } from 'react';
+import {
+  DaoIcppersQueryVariables,
+  IcpperQuery,
+  IcppersQuerySortedEnum,
+  IcppersQuerySortedTypeEnum,
+  useDaoIcppersQuery,
+} from '@/services/dao/generated';
+import { PageLoading } from '@ant-design/pro-layout';
+import { useIntl } from '@@/plugin-locale/localeExports';
+import StatCard from '@/components/StatCard';
+import { getCurrentPage, getFormatTime } from '@/utils/utils';
+import { Avatar, Space, Table, TablePaginationConfig } from 'antd';
+import { FormattedMessage } from 'umi';
+import { UserOutlined } from '@ant-design/icons';
+import { history } from '@@/core/history';
+import styles from './index.less';
 
 type DaoIcpperStatProps = {
   daoId: string;
+  tokenSymbol: string;
 };
 
-const DaoIcpperStat: React.FC<DaoIcpperStatProps> = ({ daoId }) => {
-  console.log('DaoIcpperStat', daoId);
-  return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
+const DaoIcpperStat: React.FC<DaoIcpperStatProps> = ({ daoId, tokenSymbol }) => {
+  const intl = useIntl();
+  const [queryVariable, setQueryVariable] = useState<DaoIcppersQueryVariables>({
+    daoId,
+    sorted: IcppersQuerySortedEnum.JoinTime,
+    sortedType: IcppersQuerySortedTypeEnum.Desc,
+    first: 20,
+    offset: 0,
+  });
+  const { data, loading, error } = useDaoIcppersQuery({ variables: queryVariable });
+
+  const tableChange = useCallback((pagination: TablePaginationConfig, sorter: any) => {
+    let sorted: IcppersQuerySortedEnum;
+    if (sorter && sorter.field && sorter.field.includes('size')) {
+      sorted = IcppersQuerySortedEnum.Size;
+    }
+    if (sorter && sorter.field && sorter.field.includes('income')) {
+      sorted = IcppersQuerySortedEnum.Income;
+    }
+    if (sorter && sorter.field && sorter.field.includes('jobCount')) {
+      sorted = IcppersQuerySortedEnum.JobCount;
+    }
+    if (sorter && sorter.field && sorter.field.includes('joinTime')) {
+      sorted = IcppersQuerySortedEnum.JoinTime;
+    }
+    let sortedType: IcppersQuerySortedTypeEnum;
+    if (sorter && sorter.order === 'ascend') {
+      sortedType = IcppersQuerySortedTypeEnum.Asc;
+    }
+    if (sorter && sorter.order === 'descend') {
+      sortedType = IcppersQuerySortedTypeEnum.Desc;
+    }
+    setQueryVariable((old) => ({
+      ...old,
+      first: pagination.pageSize || 10,
+      offset: ((pagination.current || 1) - 1) * (pagination.pageSize || 10),
+      sorted,
+      sortedType,
+    }));
+  }, []);
+
+  if (loading || error) {
+    return <PageLoading />;
+  }
+
+  const columns = [
+    {
+      title: <FormattedMessage id="pages.dao.component.dao_icpper.table.head.icpper" />,
+      render: (_: any, record: IcpperQuery) => (
+        <Space size="middle">
+          <Avatar size="small" icon={<UserOutlined />} />
+          <span>
+            <a
+              onClick={(event) => {
+                event.preventDefault();
+                history.push(`/job?userName=${record.user?.githubLogin}&daoId=${daoId}`);
+              }}
+            >
+              {record.user?.nickname}
+            </a>
+          </span>
+        </Space>
+      ),
+    },
+    {
+      title: <FormattedMessage id="pages.dao.component.dao_icpper.table.head.job" />,
+      dataIndex: 'jobCount',
+      sorter: true,
+    },
+    {
+      title: <FormattedMessage id="pages.dao.component.dao_icpper.table.head.size" />,
+      dataIndex: 'size',
+      sorter: true,
+    },
+    {
+      title: <FormattedMessage id="pages.dao.component.dao_icpper.table.head.income" />,
+      dataIndex: 'income',
+      sorter: true,
+    },
+    {
+      title: <FormattedMessage id="pages.dao.component.dao_icpper.table.head.join_time" />,
+      dataIndex: 'joinTime',
+      render: (_: any, record: IcpperQuery) => <>{getFormatTime(record.joinTime || 0, 'LL')}</>,
+      sorter: true,
+    },
+  ];
+
+  const statCardData = [
+    {
+      title: intl.formatMessage({ id: 'pages.dao.component.dao_icpper.stat.icpper' }),
+      number: data?.dao?.icppers?.stat?.icpperCount || 0,
+    },
+    {
+      title: intl.formatMessage({ id: 'pages.dao.component.dao_icpper.stat.job' }),
+      number: data?.dao?.icppers?.stat?.jobCount || 0,
+    },
+    {
+      title: intl.formatMessage({ id: 'pages.dao.component.dao_icpper.stat.size' }),
+      number: data?.dao?.icppers?.stat?.size || 0,
+    },
+    {
+      title: tokenSymbol,
+      number: data?.dao?.icppers?.stat?.income || 0,
+    },
+  ];
+  const dataSource = data?.dao?.icppers?.nodes || [];
+  return (
+    <>
+      <div className={styles.TabStatCard}>
+        <StatCard data={statCardData} />
+      </div>
+      <Table
+        columns={columns}
+        loading={loading}
+        rowKey={(record) => record?.user?.id || ''}
+        dataSource={dataSource as IcpperQuery[]}
+        onChange={(pagination, filters, sorter) => tableChange(pagination, sorter)}
+        pagination={{
+          pageSize: 10,
+          total: data?.dao?.icppers?.total || 0,
+          current: getCurrentPage(queryVariable.offset || 0, 10),
+        }}
+      />
+    </>
+  );
 };
 
 export default DaoIcpperStat;
