afbd94d043f885358ab304d8e156ba9a0291fdcb
fushang318 <fushang318@gmail.com>
2021-09-27 18:17:03 +0800

    UI: Optimise home page

--- src/components/IconFont/index.tsx
+++ src/components/IconFont/index.tsx
@@ -1,6 +1,6 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_2532981_jl3xxal2b8m.js',
+  scriptUrl: '//at.alicdn.com/t/font_2532981_2nisn0exph3.js',
 });
 export default IconFont;
