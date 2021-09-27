6e74de0d4f400a3a4d13b613cfeb31913514830b
bqx619 <bqx619@gmail.com>
2021-09-28 01:41:40 +0800

    fix: merge prod

--- src/pages/Dao/components/TokenConfig.tsx
+++ src/pages/Dao/components/TokenConfig.tsx
@@ -1,88 +1,179 @@
-import React, { useEffect, useState } from 'react';
-import { Tabs } from 'antd';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Button, Skeleton, Tabs } from 'antd';
 import { useIntl } from '@@/plugin-locale/localeExports';
 
 import styles from './index.less';
 import TokenCreate from '@/pages/Dao/components/token/Create';
-import { useDaoTokenConfigQuery } from '@/services/dao/generated';
-import { PageLoading } from '@ant-design/pro-layout';
+import { useDaoTokenConfigQuery, useUpdateDaoBaseInfoMutation } from '@/services/dao/generated';
 import { useModel } from '@@/plugin-model/useModel';
 import TokenCreateLP from '@/pages/Dao/components/token/CreateLP';
 import TokenAddLP from '@/pages/Dao/components/token/AddLP';
 import TokenManager from '@/pages/Dao/components/token/Manager';
 import TokenMint from '@/pages/Dao/components/token/Mint';
+import { DAOTokenConnect } from '@/services/ethereum-connect/token';
+import { ZeroAddress } from '@/services/ethereum-connect';
 
 const { TabPane } = Tabs;
 
 type TokenConfigProps = {
   daoId: string;
+  tokenSymbol: string;
 };
 
 export type TokenConfigComponentsProps = {
   daoId?: string;
   ethDAOId?: string;
   tokenAddress?: string;
+  lpPoolAddress?: string;
+  tokenContract?: DAOTokenConnect;
   setTokenAddress?: (value: string) => void;
+  setLPPoolAddress?: (value: string) => void;
   setCurrentTab?: (value: string) => void;
 };
 
-const TokenConfig: React.FC<TokenConfigProps> = ({ daoId }) => {
+const TokenConfig: React.FC<TokenConfigProps> = ({ daoId, tokenSymbol }) => {
   const intl = useIntl();
   const { data, loading, error } = useDaoTokenConfigQuery({ variables: { daoId } });
   const [tokenAddress, setTokenAddress] = useState<string>();
+  const [lpPoolAddress, setLPPoolAddress] = useState<string>('');
   const [currentTab, setCurrentTab] = useState<string>('create');
-  const { contract } = useModel('useWalletModel');
+  const { contract, network, metamaskProvider, isConnected, event$ } = useModel('useWalletModel');
+  const [updateDaoBaseInfo] = useUpdateDaoBaseInfoMutation();
 
   useEffect(() => {
     if (!data?.daoTokenConfig?.ethDaoId) return;
-    contract.daoFactory
-      .getTokenAddress(data.daoTokenConfig.ethDaoId)
-      .then((v: string) => setTokenAddress(v));
+    contract.daoFactory.getTokenAddress(data.daoTokenConfig.ethDaoId).then((v: string) => {
+      setTokenAddress(v);
+      console.log({ tokenAddress: v });
+    });
   }, [contract.daoFactory, data?.daoTokenConfig?.ethDaoId]);
 
-  if (loading || error || !tokenAddress) return <PageLoading />;
+  const tokenContract = useMemo(() => {
+    if (!tokenAddress || tokenAddress === ZeroAddress) return undefined;
+    return new DAOTokenConnect(tokenAddress, network, metamaskProvider);
+  }, [metamaskProvider, network, tokenAddress]);
+
+  useEffect(() => {
+    if (!tokenContract) return;
+    tokenContract.getLPPool().then((pd) => {
+      setLPPoolAddress(pd);
+    });
+  }, [tokenContract]);
+
+  useEffect(() => {
+    if (!tokenContract || tokenSymbol !== '') return;
+    tokenContract.getToken().then((tokenInfo) => {
+      updateDaoBaseInfo({
+        variables: {
+          id: daoId,
+          tokenAddress,
+          tokenName: tokenInfo.name,
+          tokenSymbol: tokenInfo.symbol,
+        },
+      });
+    });
+  }, [
+    daoId,
+    metamaskProvider,
+    network,
+    tokenAddress,
+    tokenContract,
+    tokenSymbol,
+    updateDaoBaseInfo,
+  ]);
+
+  const handlerMetamaskConnect = useCallback(() => {
+    event$?.emit();
+  }, [event$]);
+
+  if (loading || error || !tokenAddress) return <Skeleton active />;
 
   return (
     <>
-      <Tabs
-        className={styles.tokenConfigTabs}
-        activeKey={currentTab}
-        tabPosition={'left'}
-        onChange={setCurrentTab}
-      >
-        <TabPane tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.create' })} key="create">
-          {currentTab === 'create' && (
-            <TokenCreate
-              ethDAOId={data?.daoTokenConfig?.ethDaoId || ''}
-              tokenAddress={tokenAddress}
-              setTokenAddress={setTokenAddress}
-              daoId={daoId}
-            />
-          )}
-        </TabPane>
-        <TabPane
-          tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.create_pool' })}
-          key="createPool"
+      {!isConnected && (
+        <Button type="primary" onClick={() => handlerMetamaskConnect()}>
+          {intl.formatMessage({
+            id: 'pages.common.connect',
+          })}
+        </Button>
+      )}
+      {isConnected && (
+        <Tabs
+          className={styles.tokenConfigTabs}
+          activeKey={currentTab}
+          tabPosition={'left'}
+          onChange={setCurrentTab}
         >
-          {currentTab === 'createPool' && <TokenCreateLP tokenAddress={tokenAddress} />}
-        </TabPane>
-        <TabPane tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.add_lp' })} key="addLP">
-          {currentTab === 'addLP' && (
-            <TokenAddLP tokenAddress={tokenAddress} setCurrentTab={setCurrentTab} />
-          )}
-        </TabPane>
-        <TabPane
-          tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.manager' })}
-          key="manager"
-        >
-          {currentTab === 'manager' && <TokenManager tokenAddress={tokenAddress} />}
-        </TabPane>
-        <TabPane tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.mint' })} key="mint">
-          {currentTab === 'mint' && (
-            <TokenMint tokenAddress={tokenAddress} setCurrentTab={setCurrentTab} daoId={daoId} />
-          )}
-        </TabPane>
-      </Tabs>
+          <TabPane
+            tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.create' })}
+            key="create"
+          >
+            {currentTab === 'create' && (
+              <TokenCreate
+                ethDAOId={data?.daoTokenConfig?.ethDaoId || ''}
+                tokenAddress={tokenAddress}
+                setTokenAddress={setTokenAddress}
+                daoId={daoId}
+              />
+            )}
+          </TabPane>
+          <TabPane
+            tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.create_pool' })}
+            key="createPool"
+            disabled={!tokenAddress || tokenAddress === ZeroAddress}
+          >
+            {currentTab === 'createPool' && (
+              <TokenCreateLP
+                lpPoolAddress={lpPoolAddress}
+                tokenAddress={tokenAddress}
+                setLPPoolAddress={setLPPoolAddress}
+                tokenContract={tokenContract}
+              />
+            )}
+          </TabPane>
+          <TabPane
+            tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.add_lp' })}
+            key="addLP"
+            disabled={
+              !tokenAddress ||
+              tokenAddress === ZeroAddress ||
+              !lpPoolAddress ||
+              lpPoolAddress === ZeroAddress
+            }
+          >
+            {currentTab === 'addLP' && (
+              <TokenAddLP
+                lpPoolAddress={lpPoolAddress}
+                tokenContract={tokenContract}
+                tokenAddress={tokenAddress}
+                setCurrentTab={setCurrentTab}
+              />
+            )}
+          </TabPane>
+          <TabPane
+            tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.manager' })}
+            key="manager"
+            disabled={!tokenAddress || tokenAddress === ZeroAddress}
+          >
+            {currentTab === 'manager' && <TokenManager tokenContract={tokenContract} />}
+          </TabPane>
+          <TabPane
+            tab={intl.formatMessage({ id: 'pages.dao.config.tab.token.mint' })}
+            key="mint"
+            disabled={!tokenAddress || tokenAddress === ZeroAddress}
+          >
+            {currentTab === 'mint' && (
+              <TokenMint
+                tokenAddress={tokenAddress}
+                setCurrentTab={setCurrentTab}
+                daoId={daoId}
+                lpPoolAddress={lpPoolAddress}
+                tokenContract={tokenContract}
+              />
+            )}
+          </TabPane>
+        </Tabs>
+      )}
     </>
   );
 };
